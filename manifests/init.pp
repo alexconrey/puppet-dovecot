eee799ac1564abc94c6d3791b2ea13551a1efa7c
Alex Conrey <alex@conrey.us>
2019-02-22 13:36:07 -0600

    implement user/group definition + lmtp spool file placement

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,7 +36,10 @@ class dovecot (
   $auth_listener_userdb_user  = undef,
   $auth_listener_userdb_group = undef,
   $auth_listener_postfix      = false,
+  $lmtp_unix_listener         = 'lmtp',
   $lmtp_unix_listener_mode    = undef,
+  $lmtp_unix_listener_group   = undef,
+  $lmtp_unix_listener_user    = undef,
   # 10-ssl.conf
   $ssl                        = undef,
   $ssl_cert                   = '/etc/pki/dovecot/certs/dovecot.pem',
